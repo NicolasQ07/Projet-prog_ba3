109d1542d4967e8e98713bda50da9c4b9c724edc
Tetyana Kulakova <tetyana.kulakova@epfl.ch>
2026-09-23 14:54:16 +0200

    testing if adding files works

---
+++ test.c
@@ -0,0 +1,30 @@
+/*
+
+Semaine 1:
+- Validité des cases
+- Transformation en pixels
+- Identification des Chiffres
+
+
+
+1) Dessiner votre interface utilisateur
+• Sélection du chemin contenant le Binairo
+• Affichage du Binairo dans une image à la bonne taille
+• Les différentes options d'affichage (matlab et pdf) doivent être accessibles
+• Affichage des erreurs éventuelles
+2) Lecture d’une image .png
+• Lecture de l’image courante, ex. Binairo_6x6.png
+• Validation des dimensions (ex. 454x431)
+• Extraction des pixels de l’image (.png -> pixmap couleur)
+• Conversion de pixmap couleur –> bitmap B&W (i.e. la conversion des données de l'image en
+couleur, en données de l'image en noir et blanc car il est possible que dans l'image originale
+certains pixels ne soit pas noirs ou blancs).
+• Détection du nombre de lignes et de colonnes
+• Sélection des pixels de la case courante
+• Ecriture des pixels de la case courante dans Cell.bin
+• Lancement de votre exécutable C via la ligne de commande (avec paramètres)
+• Lecture de CellValue.txt et récupération du chiffre identifié
+• Mémorisation des chiffres ou vide identifiés (1 par case) dans une chaine de caractère de
+plusieurs lignes
+
+*/
